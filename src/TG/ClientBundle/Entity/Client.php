5f6ee91be25e7a4ef64dca05f58747cf2b38598f
technograv <nicolas.michel@technograv.fr>
2015-03-24 21:39:51 +0100

    Intégration page client + mise à jour des contraintes client

--- src/TG/ClientBundle/Entity/Client.php
+++ src/TG/ClientBundle/Entity/Client.php
@@ -53,7 +53,7 @@ class Client
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255, unique=true)
-     * @Assert\Length(min=4)
+     * @Assert\Length(min=2)
      */
     private $name;
 
